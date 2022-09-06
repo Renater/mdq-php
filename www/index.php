46d1b7403967bf116831f7826fa26ccce5727e19
Guillaume Rousse <guillaume.rousse@renater.fr>
2022-09-06 16:08:19 +0200

    add support for If-Modified-Since header

--- www/index.php
+++ www/index.php
@@ -78,14 +78,24 @@ if (isset($params[3])) {
         $xml->addAttribute("cacheDuration", $federationConfig["cacheDuration"]);
     }
 
+    $last_modified_time = filemtime($mdFile);
     $etag = '"' . md5($xml->asXML()) . '"';
-    if (isset($_SERVER['HTTP_IF_NONE_MATCH']) && $_SERVER['HTTP_IF_NONE_MATCH'] == $etag) {
+    if (
+        (
+            isset($_SERVER['HTTP_IF_NONE_MATCH']) &&
+            $_SERVER['HTTP_IF_NONE_MATCH'] == $etag
+        ) ||
+        (
+            isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
+            strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $last_modified_time
+        )
+    ) {
         http_response_code(304);
         exit();
     }
 
     header('Content-Type: application/samlmetadata+xml');
-    header('Last-Modified: ' . gmdate('D, d M Y H:i:s ', filemtime($mdFile)) . 'GMT');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s ', $last_modified_time) . 'GMT');
     header('ETag: ' . $etag);
     http_response_code(200);
     echo $xml->asXML();
