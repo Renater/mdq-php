516552315d40a6d74d1a9b6dfae8ad6c13880cb3
Guillaume Rousse <guillaume.rousse@renater.fr>
2022-09-06 16:16:42 +0200

    add caching headers support for entities list

--- www/index.php
+++ www/index.php
@@ -111,6 +111,7 @@ XML
     );
     $node = $doc->documentElement;
 
+    $last_modified_time = 0;
     foreach (explode("+", $params[1]) as $source) {
         if (!isset($config["federations"][$source])) {
             $logger->error("Unknown source: $source");
@@ -121,10 +122,32 @@ XML
             $entity_doc->load($file);
             $entity_node = $doc->importNode($entity_doc->documentElement, true);
             $node->appendChild($entity_node);
+
+            $entity_last_modified_time = filemtime($file);
+            if ($entity_last_modified_time > $last_modified_time) {
+                $last_modified_time = $entity_last_modified_time;
+            }
         }
     }
 
+    $etag = '"' . md5($doc->saveXML()) . '"';
+    if (
+        (
+            isset($_SERVER['HTTP_IF_NONE_MATCH']) &&
+            $_SERVER['HTTP_IF_NONE_MATCH'] == $etag
+        ) ||
+        (
+            isset($_SERVER['HTTP_IF_MODIFIED_SINCE']) &&
+            strtotime($_SERVER['HTTP_IF_MODIFIED_SINCE']) >= $last_modified_time
+        )
+    ) {
+        http_response_code(304);
+        exit();
+    }
+
     header('Content-Type: application/samlmetadata+xml');
+    header('Last-Modified: ' . gmdate('D, d M Y H:i:s ', $last_modified_time) . 'GMT');
+    header('ETag: ' . $etag);
     http_response_code(200);
     echo $doc->saveXML();
 }
